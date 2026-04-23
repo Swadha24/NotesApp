99ae0a81e1f2770b0fb2b6648d7b77398dedfe8a
Swadha24 <swadhapriyadutta@gmail.com>
2026-04-23 21:17:55 +0530

    Temporary open CORS fix

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,17 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({
-  origin: "https://notes-app-nu-ochre.vercel.app"
-}));
-
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
 const noteRoutes = require("./routes/noteRoutes");
 
 app.use("/", noteRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running on port ${process.env.PORT || 5000}`);
+  console.log("Server running");
 });
