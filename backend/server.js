8e58c1222a73a67a033df2cb4452d6e375127840
Swadha24 <swadhapriyadutta@gmail.com>
2026-04-23 21:13:44 +0530

    Fixed CORS for frontend

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,20 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://notes-app-nu-ochre.vercel.app"
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 const noteRoutes = require("./routes/noteRoutes");
 
 app.use("/", noteRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
-const cors = require("cors");
-
-app.use(cors({
-  origin: "https://notes-app-nu-ochre.vercel.app"
-}));
